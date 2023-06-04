8f2409b2552e3fbeb08269dff3575f831d1919d9
unknown <yakinoki>
2023-06-04 21:59:29 +0900

    add_latex

--- scipy_stats_training_2.py
+++ scipy_stats_training_2.py
@@ -1,3 +1,5 @@
+# pip install latexify-py
+import latexify
 from scipy import stats
 from scipy import integrate
 from functools import partial
@@ -15,11 +17,14 @@ def f(x):
 #1つめの返り値が積分値。2つめは推定誤差。
 print(integrate.quad(f, 1, 3))
 
+@latexify.function
 def h(x,y):
     if 0 <= x+y <=1 and 0 <= x <= 1/2:
         return 4*x*(x-y)
     else:
         return 0
+
+h
 
 print(integrate.nquad(h, [[-np.inf, np.inf],[-np.inf, np.inf]]))
 
