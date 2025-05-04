a190fe6a4cd56c74600a7d3ac022bdf3657c794f
NAOYA\bundl <yakinoki>
2025-05-04 10:46:56 +0900

    fix error

--- others/integral.py
+++ others/integral.py
@@ -1,7 +1,9 @@
 # pip install latexify-py
-import latexify
+#import latexify
 from scipy import integrate
 from functools import partial
+import numpy as np
+import warnings
 
 
 warnings.filterwarnings('ignore',category=integrate.IntegrationWarning)
@@ -16,7 +18,7 @@ def f(x):
 print(integrate.quad(f, 1, 3))
 
 @latexify.function
-def h(x,y):
+def h(x, y):
     if 0 <= x+y <=1 and 0 <= x <= 1/2:
         return 4*x*(x-y)
     else:
@@ -28,5 +30,6 @@ print(integrate.nquad(h, [[-np.inf, np.inf],[-np.inf, np.inf]]))
 
 def h_x(x):
     return integrate.quad(partial(h, x), -np.inf, np.inf)[0]
+
 def h_y(y):
     return integrate.quad(partial(h, y=y), -np.inf, np.inf)[0]
