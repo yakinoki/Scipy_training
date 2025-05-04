1c1ae33fba99061c00f72d213a6ab9b4e14e6989
NAOYA\bundl <yakinoki>
2025-05-04 11:13:10 +0900

    fix error

--- others/integral.py
+++ others/integral.py
@@ -1,26 +1,28 @@
 # pip install latexify-py
-#import latexify
+import latexify  # Added import statement
 from scipy import integrate
 from functools import partial
 import numpy as np
 import warnings
 
 
-warnings.filterwarnings('ignore',category=integrate.IntegrationWarning)
+warnings.filterwarnings('ignore', category=integrate.IntegrationWarning)
 
 def f(x):
     if 0 <= x <= 6:
-        return 3*x
+        return 3 * x
     else:
         return 0
 
-#1つめの返り値が積分値。2つめは推定誤差。
+
+# 1つめの返り値が積分値。2つめは推定誤差。
 print(integrate.quad(f, 1, 3))
+
 
 @latexify.function
 def h(x, y):
-    if 0 <= x+y <=1 and 0 <= x <= 1/2:
-        return 4*x*(x-y)
+    if 0 <= x + y <= 1 and 0 <= x <= 1 / 2:
+        return 4 * x * (x - y)
     else:
         return 0
 
